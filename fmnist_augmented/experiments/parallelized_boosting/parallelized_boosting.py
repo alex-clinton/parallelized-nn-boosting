54e53cda0774ed2743e6298c54347839fc602df3
Alex <78465604+alex-clinton@users.noreply.github.com>
2023-01-06 16:45:33 -0500

    Naming changes

--- fmnist_augmented/experiments/parallelized_boosting/parallelized_boosting.py
+++ fmnist_augmented/experiments/parallelized_boosting/parallelized_boosting.py
@@ -13,38 +13,17 @@ import os
 import time
 from keras.utils.vis_utils import plot_model
 
-'''
-Things to keep in mind/check while running tests
-1. # of epochs and epoch split
-2. training set size
-3. file that is being written to
-4. network architecture
-5. dataset
-'''
-
-#######################################
-# Variables across all script instances
-#######################################
-
 # MPI Variables
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 # Batch and test size
-bs = 40000
-ts = int(bs*.25)
+batch_size = 40000
+test_size = batch_size//4
 
 # Training splits
 first_split = 20
 second_split = 50
-
-# How to split training data to make the multiple datasets
-'''
-bs               part
---------------------------------------------
-|  ds1  |       ds2      |        ds3      |
---------------------------------------------
-'''
 
 # Training and testing data
 comb_x_train = np.load('../data/comb_x_train.npy')
@@ -56,14 +35,9 @@ y_test = np.load('../data/y_test.npy')
 
 # Auto detect train size and create the partition
 train_size = comb_x_train.shape[0]
-part = int((train_size-bs)/2 + bs)
-
-################################################
-# Network definition across all script instances
-################################################
+partition = int((train_size-batch_size)/2 + batch_size)
 
 net_layers = [128,100]
-
 # Returns new network model instance
 def create_network():
     model = keras.models.Sequential([
@@ -76,35 +50,30 @@ def create_network():
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-#####################################
-# Cases depending on taining instance
-#####################################
-
 # 1st neural network
 if rank == 0:
-
-    # Check that the data partition doesn't go out of bounds
-    assert(part < train_size)
+    # Check that the data partition is within bounds
+    assert(partition < train_size)
 
     # Train first net 
     model1 = create_network()
-    model1.fit(comb_x_train[:bs], comb_y_train[:bs], epochs=first_split)
+    model1.fit(comb_x_train[:batch_size], comb_y_train[:batch_size], epochs=first_split)
 
-    # Predict on the entire set (this won't all be needed but prevents costly reprediction), *10 is arbitrary
-    m1_pred = np.argmax(model1.predict(comb_x_train[bs:part]), axis=1)
-    m1_corr = np.where(m1_pred == comb_y_train_labels[bs:part])[0]
-    m1_incorr = np.where(m1_pred != comb_y_train_labels[bs:part])[0]
+    # Predict on the entire set (this won't all be needed but prevents costly reprediction)
+    m1_pred = np.argmax(model1.predict(comb_x_train[batch_size:partition]), axis=1)
+    m1_corr = np.where(m1_pred == comb_y_train_labels[batch_size:partition])[0]
+    m1_incorr = np.where(m1_pred != comb_y_train_labels[batch_size:partition])[0]
 
     # Picks out correct and incorrect examples
     # Account for the offset 
-    m1_corr_sampl = m1_corr[:int(bs/2)] + bs
-    m1_incorr_sampl = m1_incorr[:int(bs/2)] + bs
+    m1_corr_sampl = m1_corr[:batch_size//2] + batch_size
+    m1_incorr_sampl = m1_incorr[:batch_size//2] + batch_size
 
     # Concatinate right and wrong examples
     batch_2_x_unshuff = np.concatenate((comb_x_train[m1_corr_sampl], comb_x_train[m1_incorr_sampl]))
     batch_2_y_unshuff = np.concatenate((comb_y_train[m1_corr_sampl], comb_y_train[m1_incorr_sampl]))
 
-    assert(len(batch_2_x_unshuff) == bs)
+    assert(len(batch_2_x_unshuff) == batch_size)
 
     # Shuffle training data
     shuffle_b2 = np.random.permutation(len(batch_2_x_unshuff))
@@ -118,27 +87,26 @@ if rank == 0:
     except:
         print('Directory for training set 2 already exists')
 
-    # save second training dataset
+    # Save second training dataset
     np.save('../data/train_set2/batch_2_x.npy', batch_2_x)
     np.save('../data/train_set2/batch_2_y.npy', batch_2_y)
 
     # Eager predictions that will be used later for making dataset 3
-    m1_ds3pred = np.argmax(model1.predict(comb_x_train[part:]), axis=1)
+    m1_ds3pred = np.argmax(model1.predict(comb_x_train[partition:]), axis=1)
     np.save('../data/predictions/m1_ds3pred.npy', m1_ds3pred)
 
     # Alert 2nd process that model1 has been saved
     comm.send('\n Model1 passed on\n', dest=1, tag=0)
 
     # Finish training on first net
-    model1.fit(comb_x_train[:bs], comb_y_train[:bs], epochs=second_split)
+    model1.fit(comb_x_train[:batch_size], comb_y_train[:batch_size], epochs=second_split)
 
     # Used the fully trained model1 to make final predictions on the test data
-    m1_final_pred = model1.predict(x_test[:ts])
+    m1_final_pred = model1.predict(x_test[:test_size])
     np.save('../data/predictions/m1_final_pred.npy', m1_final_pred)
 
 elif rank == 1:
-
-    # Wait for the single from process 1 to start 
+    # Wait for the signal from process 1 to start 
     message = comm.recv(tag=0, source=0)
     print(message)
 
@@ -153,15 +121,15 @@ elif rank == 1:
     # Predictions for first 2 networks
     # Loaded from another file so that no time is wasted loading the models
     m1_ds3pred = np.load('../data/predictions/m1_ds3pred.npy')
-    m2_pred = np.argmax(model2.predict(comb_x_train[part:]), axis=1)
-    disagreements = np.where(m1_ds3pred != m2_pred)[0] + 10*bs
+    m2_pred = np.argmax(model2.predict(comb_x_train[partition:]), axis=1)
+    disagreements = np.where(m1_ds3pred != m2_pred)[0] + 10*batch_size
 
     # Make sure that our data set size is actually big enough
-    assert(len(disagreements[:bs]) == bs)
+    assert(len(disagreements[:batch_size]) == batch_size)
 
     # Third batch
-    batch_3_x = comb_x_train[disagreements[:bs]]
-    batch_3_y = comb_y_train[disagreements[:bs]]
+    batch_3_x = comb_x_train[disagreements[:batch_size]]
+    batch_3_y = comb_y_train[disagreements[:batch_size]]
 
     try:
         os.mkdir('../data/train_set3')
@@ -178,11 +146,10 @@ elif rank == 1:
     model2.fit(batch_2_x, batch_2_y, epochs=second_split)
 
     # Used the fully trained model 2 to make final predictions on the test data
-    m2_final_pred = model2.predict(x_test[:ts])
+    m2_final_pred = model2.predict(x_test[:test_size])
     np.save('../data/predictions/m2_final_pred.npy', m2_final_pred)
 
 elif rank == 2:
-
     start = time.time()
 
     # Wait for the single from process 2 to start 
@@ -200,12 +167,10 @@ elif rank == 2:
     end = time.time()
     print('Time:', str(end-start))
 
-    # Evaluation
-
     # Individual Predictions (loading other two pre-predictions)
     m1_final_pred = np.load('../data/predictions/m1_final_pred.npy')
     m2_final_pred = np.load('../data/predictions/m2_final_pred.npy')
-    m3_final_pred = model3.predict(x_test[:ts])
+    m3_final_pred = model3.predict(x_test[:test_size])
 
     # Aggregate Predicitons
     total_pred = m1_final_pred + m2_final_pred + m3_final_pred
@@ -216,27 +181,25 @@ elif rank == 2:
     m3_final_pred_class = np.argmax(m3_final_pred, axis=1) 
 
     # Calculate Accuracy
-    y_test_labels = np.where(y_test[:ts] == 1)[1]
-    print(y_test_labels.shape)
-    print(m1_final_pred.shape)
+    y_test_labels = np.where(y_test[:test_size] == 1)[1]
 
-    wrong_pred = np.where(final_pred != y_test_labels[:ts])[0]
-    m1_wrong_pred = np.where(m1_final_pred_class != y_test_labels[:ts])[0]
-    m2_wrong_pred = np.where(m2_final_pred_class != y_test_labels[:ts])[0]
-    m3_wrong_pred = np.where(m3_final_pred_class != y_test_labels[:ts])[0]
+    wrong_pred = np.where(final_pred != y_test_labels[:test_size])[0]
+    m1_wrong_pred = np.where(m1_final_pred_class != y_test_labels[:test_size])[0]
+    m2_wrong_pred = np.where(m2_final_pred_class != y_test_labels[:test_size])[0]
+    m3_wrong_pred = np.where(m3_final_pred_class != y_test_labels[:test_size])[0]
 
-    accuracy = (ts-len(wrong_pred))/ts
-    m1_accuracy = (ts-len(m1_wrong_pred))/ts
-    m2_accuracy = (ts-len(m2_wrong_pred))/ts
-    m3_accuracy = (ts-len(m3_wrong_pred))/ts
+    accuracy = (test_size-len(wrong_pred))/test_size
+    m1_accuracy = (test_size-len(m1_wrong_pred))/test_size
+    m2_accuracy = (test_size-len(m2_wrong_pred))/test_size
+    m3_accuracy = (test_size-len(m3_wrong_pred))/test_size
 
     print('Accuracy:', accuracy)
 
-    # Write results to results.txt
+    # Write results to file
     with open('parallel_boosting/parallel_boosting_results.txt', 'a') as f:
         f.write(f'FMNIST\t accuracy: {accuracy:.3f}\t time: {end-start:.2f}\t split: {first_split}/{second_split}\n')
-        # Optional: individual evalutaions
+        # Individual evalutaions
         f.write(f'm1 acc: {m1_accuracy:.3f}\t m2 acc: {m2_accuracy:.3f}\t m3 acc: {m3_accuracy:.3f}\n')
-        # Optional: network architecture and training data stats
-        f.write(f'layer nodes: {net_layers}\t batch size: {bs}\t test size: {ts}\n')
+        # Network architecture and training data stats
+        f.write(f'layer nodes: {net_layers}\t batch size: {batch_size}\t test size: {test_size}\n')
         f.write('\n')
